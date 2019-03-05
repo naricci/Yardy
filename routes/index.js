6a1b4e9ee87559d5d7278cffbb34be7300cac34f
001408945 <001408945@N208D3.labs.neit.edu>
2019-03-04 21:12:25 -0500

    early script for displaying yahd sales

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const yardsaleController = require('../controllers/yardsaleController');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
 	res.render('index', { title: 'Welcome!' });
 });
+
+/*try to display some yahdsales*/
+router.get('/yardsale', yardsaleController.all_yardsales);
+
+router.get('/yardsale_by_date', yardsaleController.all_yardsales_sorted);
 
 module.exports = router;
