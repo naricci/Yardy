5e8d119fc6962fa4b08a15997951c3f7467b223a
jmvbogo <jmvbogo@gmail.com>
2019-03-09 19:26:45 -0500

    Little changes

--- routes/yardsales.js
+++ routes/yardsales.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-const yardsaleController = require('../controllers/yardsaleController');
+// const yardsaleController = require('../controllers/yardsaleController');
 
-router.get('/', yardsaleController.all_yardsales);
+// router.get('/', yardsaleController.all_yardsales);
 
-module.exports = router;
+// router.get('/', yardsaleController.all_yardsales_sorted);
+
+// module.exports = router;
