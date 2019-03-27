e34f0d7aded19d55f0251b153665afb8ae568f61
Nick Ricci <naricci@email.neit.edu>
2019-03-27 19:43:52 -0400

    tweaked panel for user profile pages

--- public/js/index_map.js
+++ public/js/index_map.js
@@ -27,7 +27,7 @@ function initMap() {
 		'<h1 id="firstHeading" class="firstHeading">Address  </h1>'+
 		'<h1 id="firstHeading" class="firstHeading">  Date</h1>'+
 		'<div id="bodyContent">'+
-		'<p class="image is-64x64"><img src="https://bulma.io/images/placeholders/128x128.png"></p>'+
+		'<p class="image is-64x64"><images src="https://bulma.io/images/placeholders/128x128.png"></p>'+
 		'<p>Yard sale info.</p>'+
 		'</div>'+
 		'</div>';
