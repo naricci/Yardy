5ad104aa917cabf350eee36577e2025397ca0801
Nick Ricci <naricci@email.neit.edu>
2019-04-24 19:42:35 -0400

    search logic/error handling

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -19,30 +19,30 @@ exports.index = (req, res, next) => {
 		});
 };
 
-// TODO - Displaying results not working
 exports.search = (req, res, next) => {
-	debug('Searching for yard sales.');
-	var params = req.query.address;
-	let paramsLike = '/'+params+'/i';
-	Yardsale
-		.find()
-		.where({ city: params })
-		.populate('user')
-		.sort([['date', 'ascending']])
-		.exec((err, list_yardsales) => {
-			if (err) return next(err);
-			if (list_yardsales === null) { // No yardsales.
-				err = new Error('Yardsale not found');
-				err.status = 404;
-				return next(err);
-			}
-			Object.keys(list_yardsales).forEach((item) => {
-				debug(item);
+	if (req.query.address !== '' && req.method === 'GET') {
+		debug('Searching for yard sales.');
+		let params = req.query.address;
+		Yardsale
+			.find()
+			.where({ city: params })
+			.populate('user')
+			.sort([['date', 'ascending']])
+			.exec((err, list_yardsales) => {
+				if (err) return next(err);
+				if (list_yardsales === null) { // No yardsales.
+					err = new Error('Yardsale not found');
+					err.status = 404;
+					return next(err);
+				}
+				Object.keys(list_yardsales).forEach((item) => {
+					debug(item);
+				});
+				// Successful, so render
+				res.render('index', {
+					title: 'Yardy Search Results',
+					yardsale_list: list_yardsales
+				});
 			});
-			// Successful, so render
-			res.render('index', {
-				title: 'Yardy Search Results',
-				yardsale_list: list_yardsales
-			});
-		});
+	}
 };
