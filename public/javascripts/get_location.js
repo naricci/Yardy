05d43b46a9c1796918f2f1c9dd4825c8248cfbcc
Nicholas Ricci <naricci@email.neit.edu>
2019-06-27 13:47:33 -0400

    reverse geocoder function to get current location from lat/long coordinates

--- public/javascripts/get_location.js
+++ public/javascripts/get_location.js
@@ -1,15 +1,16 @@
-var google;
-const searchParams = document.getElementById('searchParams');
-const findMe = document.getElementById('find-me');
-findMe.addEventListener('click', getLocation, { passive: true });
-
 /**
  *  Geocoding API Request Format
  *  https://maps.googleapis.com/maps/api/geocode/outputFormat?parameters
  */
 
+var google;
+var searchParams = document.getElementById('searchParams');
+const findMe = document.getElementById('find-me');
+findMe.addEventListener('click', getLocation, { passive: true });
+
 const userLat = localStorage.getItem('lat');
 const userLng = localStorage.getItem('lng');
+// const userLatLng = userLat + ', ' + userLng;
 
 // DATE STUFF
 // var date = new Date;
@@ -39,7 +40,7 @@ function showPosition(position) {
 	// console.log(lat + ', ' + lng);
 	localStorage.setItem('lat', lat);
 	localStorage.setItem('lng', lng);
-	// searchParams.value = lat + ', ' + lng;
+	searchParams.value = lat + ', ' + lng;
 	// TODO - Add address to search bar
 }
 
@@ -73,30 +74,79 @@ function initMyMap() {
 		zoom: 10
 	});
 
-	let locations = [
-		new google.maps.LatLng(parseFloat(userLat), parseFloat(userLng))
-	];
+	var geocoder = new google.maps.Geocoder;
+	var infowindow = new google.maps.InfoWindow;
 
-	locations.forEach(location => {
-		let marker = new google.maps.Marker({
-			position: location,
-			map: myMap,
-			title:'Yard Sale'
-		});
+	geocodeLatLng(geocoder, myMap, infowindow);
 
-		marker.addListener('mouseover', () => {
-			infoWindow.open(myMap, marker);
-		},
-		{ passive: true });
-	});
+	// let locations = [
+	// 	new google.maps.LatLng(parseFloat(userLat), parseFloat(userLng))
+	// ];
+	//
+	// locations.forEach(location => {
+	// 	let marker = new google.maps.Marker({
+	// 		position: location,
+	// 		map: myMap,
+	// 		title:'Yard Sale'
+	// 	});
 
-	// TODO - Add loop to display all yardsales on the map as markers
-	let contentString =
-		'<div id="content">' +
-		'<p>You are here!</p>' +
-		'</div>';
+	// 	marker.addListener('mouseover', () => {
+	// 		infoWindow.open(myMap, marker);
+	// 	},
+	// 	{ passive: true });
+	// });
+	//
+	// // TODO - Add loop to display all yardsales on the map as markers
+	// let contentString =
+	// 	'<div id="content">' +
+	// 	'<center>YOU ARE HERE!</center>' +
+	// 	'<br>' +
+	// 	'<p>Lat: ' + userLat + '</p>' +
+	// 	'<p>Long: ' + userLng + '</p>' +
+	// 	'</div>';
+	//
+	// let infoWindow = new google.maps.InfoWindow({
+	// 	content: contentString
+	// });
+}
 
-	let infoWindow = new google.maps.InfoWindow({
-		content: contentString
+// For Reverse Geocoding
+// function initMap() {
+// 	var myMap = new google.maps.Map(document.getElementById('map'), {
+// 		zoom: 8,
+// 		center: {lat: 40.731, lng: -73.997}
+// 	});
+// 	var geocoder = new google.maps.Geocoder;
+// 	var infowindow = new google.maps.InfoWindow;
+//
+// 	document.getElementById('submit').addEventListener('click', function() {
+// 		geocodeLatLng(geocoder, myMap, infowindow);
+// 	});
+// }
+
+function geocodeLatLng(geocoder, map, infowindow) {
+	// var input = document.getElementById('latlng').value;
+	var input = document.getElementById('searchParams').value;
+	var latlngStr = input.split(',', 2);
+	var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+	geocoder.geocode({'location': latlng}, function(results, status) {
+		if (status === 'OK') {
+			if (results[0]) {
+				map.setZoom(11);
+				var marker = new google.maps.Marker({
+					position: latlng,
+					map: map
+				});
+				// Display results address in search bar
+				searchParams.value = results[0].formatted_address;
+
+				infowindow.setContent(results[0].formatted_address);
+				infowindow.open(map, marker);
+			} else {
+				window.alert('No results found');
+			}
+		} else {
+			window.alert('Geocoder failed due to: ' + status);
+		}
 	});
 }
