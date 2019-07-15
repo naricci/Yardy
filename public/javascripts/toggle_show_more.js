2c5869c815a76c8dec0f5ed51812345a9b87642a
Nicholas Ricci <naricci@email.neit.edu>
2019-07-15 16:46:37 -0400

    front-end stuff

---
+++ public/javascripts/toggle_show_more.js
@@ -0,0 +1,11 @@
+// var isMobile = false;
+// var browserType = 'desktop browser';
+// if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+// 	isMobile = true;
+// 	browserType = 'mobile browser';
+// }
+// else {
+// 	isMobile = false;
+// 	browserType = 'desktop browser';
+// }
+// // console.log(browserType);
